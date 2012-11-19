ebcef631002816d1504b722e648b51f6718329da
Tito Miguel Costa <github@titomiguelcosta.com>
2012-11-19 21:30:30 +0000

    fixed translations and validation

--- Admin/BannerAdmin.php
+++ Admin/BannerAdmin.php
@@ -5,8 +5,9 @@ namespace Zorbus\BannerBundle\Admin;
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Validator\Constraints\MaxLength;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class BannerAdmin extends Admin
 {
@@ -14,7 +15,12 @@ class BannerAdmin extends Admin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-                ->add('name')
+                ->add('name', null, array(
+                    'constraints' => array(
+                        new NotBlank(),
+                        new MaxLength(array('limit' => 255))
+                    )
+                ))
                 ->add('description', 'textarea', array(
                     'required' => false,
                     'attr' => array('class' => 'ckeditor')
@@ -36,16 +42,6 @@ class BannerAdmin extends Admin
         $listMapper
                 ->addIdentifier('name')
                 ->add('enabled')
-        ;
-    }
-
-    public function validate(ErrorElement $errorElement, $object)
-    {
-        $errorElement
-                ->with('name')
-                ->assertNotBlank()
-                ->assertMaxLength(array('limit' => 255))
-                ->end()
         ;
     }
 
